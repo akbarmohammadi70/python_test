eadee948658bcd7bd58ce8f5cb74fdfd098e0b6e
Akbar Mohammadi <55222481+akbarmohammadi70@users.noreply.github.com>
2024-04-04 20:28:55 +0330

    Added extended templated and Dynamic usls

--- website/views.py
+++ website/views.py
@@ -8,5 +8,7 @@ def about_view(request):
     return render(request, 'website/about.html')
 def contact_view(request):
     return render(request, 'website/contact.html')
+def elements_view(request):
+    return render(request, 'website/elements.html')
 
 

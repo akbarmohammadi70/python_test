2c84bfc3541485e859998f3faab7e5d30f95c51a
Akbar Mohammadi <55222481+akbarmohammadi70@users.noreply.github.com>
2024-04-10 19:18:09 +0330

    complete pages stracture

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from blog.views import *
 app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='index'),
-    path('single', blog_single, name='single'),
+    path('<int:pid>', blog_single, name='single'),
     # path('<str:name>/lastname/<str:family_name>/age/<int:age>' ,test, name='test'),
-    path('post-<int:pid>' ,test, name='test'),
+    # path('post-<int:pid>' ,test, name='test'),
 
 
 ]

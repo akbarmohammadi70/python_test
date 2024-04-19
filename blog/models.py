f073a8d1d67d37e1cfe3520d35bf84acb2116597
Akbar Mohammadi <55222481+akbarmohammadi70@users.noreply.github.com>
2024-04-19 16:18:30 +0330

    session7

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    # image
+    image = models.ImageField(upload_to ='blog/', default ='blog/default.jpg')
     title   = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
-    # author
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-
     class Meta:
         ordering = ['created_date']
-        verbose_name_plural = 'پست ها'
-        
+        verbose_name_plural = 'پست ها'    
     def __str__(self):
         return "{} - {}".format(self.title,self.id)
+    def snippets(self):
+        return self.content[:100] + '...'
+
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+    print(kwargs)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        post = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3)
+
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -14,9 +29,26 @@ def blog_single(request, pid):
 
 
 # def test(request, name, family_name, age):
-def test(request, pid):
+def test(request):
     # context = {'name': name, 'family_name': family_name, 'age': age}
     # post = Post.objects.get(id=pid)
-    post = get_object_or_404(Post, pk=pid)
-    context = {'post': post}
-    return render(request, 'test.html', context)
+    # post = get_object_or_404(Post, pk=pid)
+    # context = {'post': post}
+    return render(request, 'test.html')
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def blog_search(request):
+    #print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        # print(request.GET.get('s'))
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+    

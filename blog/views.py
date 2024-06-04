3fefbcf2cf6347cb74411e33e0cf211aa466d3b6
Akbar Mohammadi <55222481+akbarmohammadi70@users.noreply.github.com>
2024-06-04 14:30:53 +0330

    Tamrin6

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    now = timezone.now()
+    posts = Post.objects.filter(published_date__lte=now)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(status=1)
+    now = timezone.now()
+    posts = Post.objects.filter(published_date__lte=now)
     post = get_object_or_404(posts, pk=pid)
+    post.counted_view += 1
+    post.save()
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 

78cf1c8d3777d3fbd0c6bae8ce9b079617d096f6
Akbar Mohammadi <55222481+akbarmohammadi70@users.noreply.github.com>
2024-06-07 17:11:31 +0330

    fix problem in practice 7

--- website/templatetags/latest_posts.py
+++ website/templatetags/latest_posts.py
@@ -8,6 +8,6 @@ register = template.Library()
 
 @register.inclusion_tag('website/latest_posts.html')
 def latest_posts(count=6):
-    posts = Post.objects.filter(status=1, created_date__lte=timezone.now()).order_by('-created_date')[:count]
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-published_date')[:count]
     return {'latest_posts': posts}
 
